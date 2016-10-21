f7eca33e2e325ef51b8aeed251cdbb6ce7917baa
熊林PD10 <sfrost@live.cn>
2016-10-21 11:38:43 +0800

    虚机杀毒软件占用内存太高

---
+++ FROST.WeixinQYH/AdvancedAPI/Message/MessageApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FROST.WeixinQYH.AdvancedAPI {
+    public class MessageApi {
+        /// <summary>
+        /// POST发送消息的URL
+        /// </summary>
+        protected static string PostUrl = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=";
+        public static PostMessageResult MessageSendText(string accessToken, string toUser, string toParty, string toTag, int agentId, string contEnt, int saFe = 0) {
+            var data = new {
+                touser = toUser,
+                topart = toParty,
+                totag = toTag,
+                msgtype = "text",
+                agentid = agentId,
+                text = new {
+                    content = contEnt
+                },
+                safe = saFe
+            };
+
+        }
+    }
+}
---
+++ FROST.WeixinQYH/AdvancedAPI/Message/MessageResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FROST.WeixinQYH.AdvancedAPI {
+    /// <summary>
+    /// 如果无权限或收件人不存在，则本次发送失败；如果未关注，发送仍然执行。两种情况下均返回无效的部分（注：由于userid不区分大小写，返回的列表都统一转为小写）。
+    /// </summary>
+    public class PostMessageResult {
+        public int errcode { get; set; }
+        public string errmsg { get; set; }
+        public string invaliduser { get; set; }
+        public string invalidparty { get; set; }
+        public string invalidtag { get; set; }
+    }
+
+}
